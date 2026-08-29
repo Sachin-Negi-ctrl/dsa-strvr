a2ad4b83287f62bf09a0b2a308208ce385212e82
Sachin-Negi-ctrl <sachinnegizxzx@gmail.com>
2026-08-29 10:14:32 +0530

    Added pattern5

---
+++ pattern5.java
@@ -0,0 +1,29 @@
+/*
+In this program we will print:
+* * * * *
+* * * * 
+* * * 
+* * 
+*
+up until n
+*/
+
+import java.util.Scanner;
+
+public class pattern5 {
+    public static void pattern(int n){
+        for(int i=n; i>=1; i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the number of lines:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern(n);
+    }
+}
+
