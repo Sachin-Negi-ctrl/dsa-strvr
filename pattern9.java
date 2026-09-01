490b09051add1670b9362c5bc9422dcc4602ba29
Sachin-Negi-ctrl <sachinnegizxzx@gmail.com>
2026-09-01 15:04:35 +0530

    Added pattern9 combination of two patterns

---
+++ pattern9.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class pattern9{
+public static void pattern1(int n){
+
+    for(int i=0; i<n; i++){
+        //for leading space
+        for(int j=1; j<n-i; j++){
+            System.out.print(" ");
+        }
+        //for stars
+        for(int k=0; k<2*i+1; k++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
+
+    public static void pattern2(int n){
+        //for rows
+        for(int i=0; i<n; i++){
+            //for space
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            //for stars
+            for(int k=i; k<n*2-i-1; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the number of lines:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern1(n);
+        pattern2(n);
+    }
+}
