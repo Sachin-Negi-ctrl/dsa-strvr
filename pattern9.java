98e884144e51a38369af4b7d5fae320af91b1cfb
Sachin-Negi-ctrl <sachinnegizxzx@gmail.com>
2026-09-01 21:47:19 +0530

    Added the new logic for the pattern

--- pattern9.java
+++ pattern9.java
@@ -1,6 +1,38 @@
+/*
+In this program we will print the pattern:
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+*/
+
+
 import java.util.Scanner;
 
 public class pattern9{
+     public static void pattern(int n){
+        //for rows
+        for(int i=1; i<n*2; i++){
+            if(i<=n){
+                for(int j=1; j<=i; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+            else{
+                for(int k=1; k<=n*2-i; k++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
 public static void pattern1(int n){
 
     for(int i=0; i<n; i++){
@@ -35,7 +67,8 @@ public static void pattern1(int n){
         System.out.println("Enter the number of lines:");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        pattern1(n);
-        pattern2(n);
+        // pattern1(n);
+        // pattern2(n);
+        pattern(n);
     }
 }
